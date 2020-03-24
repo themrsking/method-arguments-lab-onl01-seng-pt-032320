2693e98f2fbb4ec6b5a17a3c9233883d9599e5e6
Stefani King <themrsking@gmail.com>
2020-03-24 02:42:33 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction #(name =Dan)
   puts "Hi, my name is #{Dan}."
 end
+
+def introduction_with_name_and_language #(language=ember)
+puts "Hi, my name is "#{Dan}" and I am learning to program in #{ember}"
+end
